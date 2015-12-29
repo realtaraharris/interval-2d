a0794617744eb15d902b855f6a90643590761f0a
Max Harris <harris.max@gmail.com>
2015-12-29 10:50:27 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+var fc = require('fc');
+
+var ctx = fc(function (dt) {
+  ctx.beginPath();
+  ctx.moveTo(0, 1);
+  ctx.lineTo(10, 10);
+  ctx.closePath();
+  ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+  ctx.strokeStyle = 'black';
+  ctx.stroke();
+  ctx.fill();
+  ctx.dirty();
+});
