d7d114276180d7ec5c43dfc9e6b17d9a21ab77cd
Elijah Insua <tmpvar@gmail.com>
2016-01-02 21:28:29 -0500

    reduce garbage + speedup rect rendering

--- main.js
+++ main.js
@@ -8,8 +8,13 @@ var circleRadius = [5000, 5000];
 var side = 500;
 var max = Math.max;
 
+var scratch0 = [0, 0];
+var scratch1 = [0, 0];
+var scratchx = [0, 0];
+var scratchy = [0, 0];
+var circleOut = [0, 0];
 function circle (x, y) {
-  return isub(iadd(imul(x, x), imul(y, y)), circleRadius);
+  return isub(iadd(imul(x, x, scratch0), imul(y, y, scratch1), scratch0), circleRadius, circleOut);
 }
 
 function crossesZero (interval) {
@@ -20,8 +25,13 @@ function crossesZero (interval) {
   );
 }
 
-function middle (interval) {
-  return (interval[0] + interval[1]) / 2;
+function middle (l, u) {
+  return (l + u) * 0.5;
+}
+
+function iset(out, l, u) {
+  out[0] = l;
+  out[1] = u;
 }
 
 function box (lx, ly, ux, uy, ctx, scale, depth) {
@@ -29,32 +39,37 @@ function box (lx, ly, ux, uy, ctx, scale, depth) {
   var size = Math.max(ux - lx, uy - ly) * scale
   if (size < 1) return maxDepth;
 
-  var tmp = circle([lx, ux], [ly, uy]);
-  if (crossesZero(tmp)) {
-    var midx = middle([lx, ux]);
-    var midy = middle([ly, uy]);
 
-    ctx.beginPath()
-      ctx.strokeStyle = 'rgba(45,100,200, 0.5)';
-      ctx.fillStyle = 'rgba(255, 255, 255, 0.01)';
-      ctx.rect(lx, ly, (ux - lx), (uy - ly));
-    ctx.closePath()
-    ctx.stroke();
-    ctx.fill();
+  iset(scratchx, lx, ux);
+  iset(scratchy, ly, uy);
 
-    if (crossesZero(circle([midx, ux], [midy, uy]))) { // upper-right
+  if (crossesZero(circle(scratchx, scratchy))) {
+    var midx = middle(lx, ux);
+    var midy = middle(ly, uy);
+
+    ctx.strokeRect(lx, ly, (ux - lx), (uy - ly));
+
+    iset(scratchx, midx, ux);
+    iset(scratchy, midy, uy);
+    if (crossesZero(circle(scratchx, scratchy))) { // upper-right
       maxDepth = max(maxDepth, box(midx, midy, ux, uy, ctx, scale, depth + 1));
     }
 
-    if (crossesZero(circle([lx, midx], [midy, uy]))) { // upper-left
+    iset(scratchx, lx, midx);
+    iset(scratchy, midy, uy);
+    if (crossesZero(circle(scratchx, scratchy))) { // upper-left
       maxDepth = max(maxDepth, box(lx, midy, midx, uy, ctx, scale, depth + 1));
     }
 
-    if (crossesZero(circle([lx, midx], [ly, midy]))) { // lower-right
+    iset(scratchx, lx, midx);
+    iset(scratchy, ly, midy);
+    if (crossesZero(circle(scratchx, scratchy))) { // lower-right
       maxDepth = max(maxDepth, box(lx, ly, midx, midy, ctx, scale, depth + 1));
     }
 
-    if (crossesZero(circle([midx, ux], [ly, midy]))) { // lower-left
+    iset(scratchx, midx, ux);
+    iset(scratchy, ly, midy);
+    if (crossesZero(circle(scratchx, scratchy))) { // lower-left
       maxDepth = max(maxDepth, box(midx, ly, ux, midy, ctx, scale, depth + 1));
     }
   }
@@ -73,6 +88,8 @@ window.addEventListener('mousewheel', function(e) {
 })
 
 var ctx = fc(function (dt) {
+  ctx.clear('white');
+  ctx.strokeStyle = 'rgba(45,100,200, 0.5)';
   center(ctx);
   ctx.scale(mouse.zoom, mouse.zoom)
   ctx.lineWidth = 1/mouse.zoom
