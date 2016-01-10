b95695e45d3b13cc33a2104bb33990d53a5a5b0e
Elijah Insua <tmpvar@gmail.com>
2016-01-09 23:09:11 -0500

    colorize the tree depth

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var iadd = require('interval-add');
 var isub = require('interval-subtract');
 var imul = require('interval-multiply');
 var imin = require('interval-min');
+var colorLerp = require('color-lerp');
 
 var max = Math.max;
 var min = Math.min;
@@ -153,20 +154,33 @@ function opicut(a, b) {
   )
 }
 
-function inout(ctx, r, ix, iy) {
+
+var depthColors = colorLerp('#00FF00', '#FF0000', 10);
+console.log(depthColors)
+function inout(ctx, r, ix, iy, depth) {
+  var scale = mouse.zoom;
   ctx.save()
+    ctx.lineWidth = 1/scale;
     var size = Math.max(ix[1] - ix[0], iy[1] - iy[0]);
 
-    if (r[0] < 0 && r[1] < 0) {
-      return r;
-      ctx.fillStyle = "hsla(14, 100%, 55%, .75)"
+    if (r[0] <= 0 && r[1] <= 0) {
+      // return r;
+      ctx.fillStyle = depthColors[depth];//"hsla(14, 100%, 55%, 1)"
+      ctx.fillRect(ix[0], iy[0], (ix[1] - ix[0]), (iy[1] - iy[0]));
+
+      r = [10, 10];
+
     } else if (r[0] <= 0 && r[1] >= 0 && size < (1 / mouse.zoom)) {
       ctx.fillStyle = 'white'
+    ctx.fillRect(ix[0]+1/scale, iy[0]+1/scale, (ix[1] - ix[0])-2/scale, (iy[1] - iy[0])-2/scale);
+
     } else {
-      return r;
-      ctx.fillStyle = "#000"
+
+      // return r;
+      // ctx.strokeStyle = "yellow"
+      ctx.strokeStyle = depthColors[depth];//'rgb(' + Math.floor((depth/10)*255) + ', 0, 0)';
+      ctx.strokeRect(ix[0], iy[0], (ix[1] - ix[0]), (iy[1] - iy[0]));
     }
-    ctx.fillRect(ix[0], iy[0], (ix[1] - ix[0]), (iy[1] - iy[0]));
 
   ctx.restore();
 
@@ -176,9 +190,11 @@ function inout(ctx, r, ix, iy) {
 var scratchx = [0, 0];
 var scratchy = [0, 0];
 function box (inputShapes, translation, lx, ly, ux, uy, ctx, scale, depth, fn) {
+  ctx.fillStyle = "black";
+  ctx.fillRect(lx, ly, ux-lx, ux-lx);
+
   var maxDepth = depth;
   var size = Math.max(ux - lx, uy - ly) * scale
-
   if (size < 1) {
     return maxDepth;
   }
@@ -192,7 +208,7 @@ function box (inputShapes, translation, lx, ly, ux, uy, ctx, scale, depth, fn) {
 
   var upperRightShapes = []
   r = fn(depth, inputShapes, scratchx, scratchy, translation, upperRightShapes);
-  inout(ctx, r, scratchx, scratchy);
+  r = inout(ctx, r, scratchx, scratchy, depth);
   if (crossesZero(r)) { // upper-right
     maxDepth = max(maxDepth,
       box(upperRightShapes, translation, midx, midy, ux, uy, ctx, scale, depth + 1, fn)
@@ -203,7 +219,7 @@ function box (inputShapes, translation, lx, ly, ux, uy, ctx, scale, depth, fn) {
   iset(scratchy, midy, uy);
   var upperLeftShapes = []
   r = fn(depth, inputShapes, scratchx, scratchy, translation, upperLeftShapes);
-  inout(ctx, r, scratchx, scratchy);
+  r = inout(ctx, r, scratchx, scratchy, depth);
   if (crossesZero(r)) { // upper-left
     maxDepth = max(maxDepth,
       box(upperLeftShapes, translation, lx, midy, midx, uy, ctx, scale, depth + 1, fn)
@@ -214,7 +230,7 @@ function box (inputShapes, translation, lx, ly, ux, uy, ctx, scale, depth, fn) {
   iset(scratchy, ly, midy);
   var lowerRightShapes = [];
   r = fn(depth, inputShapes, scratchx, scratchy, translation, lowerRightShapes);
-  inout(ctx, r, scratchx, scratchy);
+  r = inout(ctx, r, scratchx, scratchy, depth);
   if (crossesZero(r)) { // lower-right
     maxDepth = max(maxDepth,
       box(lowerRightShapes, translation, lx, ly, midx, midy, ctx, scale, depth + 1, fn)
@@ -225,7 +241,7 @@ function box (inputShapes, translation, lx, ly, ux, uy, ctx, scale, depth, fn) {
   iset(scratchy, ly, midy);
   var lowerLeftShapes = [];
   r = fn(depth, inputShapes, scratchx, scratchy, translation, lowerLeftShapes);
-  inout(ctx, r, scratchx, scratchy);
+  r = inout(ctx, r, scratchx, scratchy, depth);
   if (crossesZero(r)) { // lower-left
     maxDepth = max(maxDepth,
       box(lowerLeftShapes, translation, midx, ly, ux, midy, ctx, scale, depth + 1, fn)
@@ -235,35 +251,42 @@ function box (inputShapes, translation, lx, ly, ux, uy, ctx, scale, depth, fn) {
   return maxDepth;
 }
 
-var mouse = { zoom: 1, down: false, translate: [0, 0] }
+var mouse = { zoom: 1, down: false, translate: [0, 0], pos: [0, 0] }
 window.addEventListener('mousewheel', function(e) {
   ctx.dirty();
   mouse.zoom += e.wheelDelta / 500;
   if (mouse.zoom < .1) {
     mouse.zoom = .1;
   }
+  mouse.pos[0] = (e.clientX - ctx.canvas.width / 2) / mouse.zoom;
+  mouse.pos[1] = (e.clientY - ctx.canvas.height / 2) / mouse.zoom;
   e.preventDefault();
 })
 
 window.addEventListener('mousedown', function(e) { mouse.down = [e.clientX, e.clientY]; })
 window.addEventListener('mouseup', function(e) {
   mouse.down = false;
-  addShape(
-    (e.clientX - ctx.canvas.width / 2) / mouse.zoom,
-    (e.clientY - ctx.canvas.height / 2) / mouse.zoom,
-    10,
-    Math.random() * 100
-  );
+  addShape(mouse.pos[0], mouse.pos[1], 10, Math.random() * 100);
 })
 window.addEventListener('mousemove', function(e) {
+  mouse.pos[0] = (e.clientX - ctx.canvas.width / 2) / mouse.zoom - mouse.translate[0];
+  mouse.pos[1] = (e.clientY - ctx.canvas.height / 2) / mouse.zoom - mouse.translate[1];
+
   if (mouse.down) {
-    addShape(
-      (e.clientX - ctx.canvas.width / 2) / mouse.zoom,
-      (e.clientY - ctx.canvas.height / 2) / mouse.zoom,
-      10,
-      Math.random() * 100
-    );
+    addShape(mouse.pos[0], mouse.pos[1], 10, Math.random() * 100);
   }
+})
+
+
+var keyboard = {}
+window.addEventListener('keydown', function(e) {
+  keyboard[e.which] = true;
+  ctx.dirty()
+  e.preventDefault();
+})
+window.addEventListener('keyup', function(e) {
+  keyboard[e.which] = false;
+  ctx.dirty()
 })
 
 function addShape(cx, cy, width, height) {
@@ -284,22 +307,22 @@ function addShape(cx, cy, width, height) {
 
 var shapes = [];
 
-addShape(10, 10, 10, 50)
-addShape(100, 100, 10, 50)
+// addShape(10, 10, 10, 50)
+// addShape(100, 100, 10, 50)
 
-// for (var x = -500; x<=500; x+=20) {
-//   for (var y = -200; y<=200; y+=20) {
-//     shapes.push([x, y, 10])
+// for (var x = -500; x<=500; x+=10) {
+//   for (var y = -200; y<=200; y+=10) {
+//     addShape(x, y, 10, 10);//shapes.push([x, y, 10])
 //   }
 // }
 
-setInterval(function() {
-  mat3.rotate(shapes[0].transform, shapes[0].transform, Math.PI/100)
-  var s = 1 - Math.sin(Date.now() / 1000) / 50;
-  mat3.scale(shapes[0].transform, shapes[0].transform, [s, s])
+// setInterval(function() {
+//   mat3.rotate(shapes[0].transform, shapes[0].transform, Math.PI/100)
+//   var s = 1 - Math.sin(Date.now() / 1000) / 50;
+//   mat3.scale(shapes[0].transform, shapes[0].transform, [s, s])
 
-  ctx.dirty()
-}, 16);
+//   ctx.dirty()
+// }, 16);
 
 
 
@@ -307,6 +330,7 @@ var translation = [0, 0];
 var inverted = mat3.create();
 
 var ctx = fc(function (dt) {
+  ctx.save()
   var maspect = max(ctx.canvas.height, ctx.canvas.width);
   var hw = (maspect / 2) / mouse.zoom;
   var hh = (maspect / 2) / mouse.zoom;
@@ -316,6 +340,22 @@ var ctx = fc(function (dt) {
   var ux =  hw;
   var uy =  hh;
 
+  if (keyboard[39]) {
+    mouse.translate[0] += 10 / mouse.zoom;
+  }
+
+  if (keyboard[37]) {
+    mouse.translate[0] -= 10 / mouse.zoom;
+  }
+
+  if (keyboard[40]) {
+    mouse.translate[1] -= 10 / mouse.zoom;
+  }
+
+  if (keyboard[38]) {
+    mouse.translate[1] += 10 / mouse.zoom;
+  }
+
   translation[0] = mouse.translate[0];
   translation[1] = mouse.translate[1];
 
@@ -324,11 +364,6 @@ var ctx = fc(function (dt) {
   var localShapes = []
   var groups = [[], [], [], [], [], [], [], [], [], [], []];
   evaluateScene(0, shapes, [lx, ux], [ly, uy], translation, localShapes)
-
-
-  ctx.fillStyle = "white";
-  ctx.font = "12px monospace"
-  ctx.fillText('shapes: ' + localShapes.length + '/' + shapes.length, 10, 20);
 
   console.clear()
   console.time('render')
@@ -391,4 +426,10 @@ var ctx = fc(function (dt) {
   ctx.strokeStyle = "#f0f"
   ctx.strokeRect(lx, ly, ux - lx, uy - ly);
   console.timeEnd('render')
+  ctx.restore()
+
+  ctx.fillStyle = "white";
+  ctx.font = "12px monospace"
+  ctx.fillText('shapes: ' + localShapes.length + '/' + shapes.length, 10, 20);
+
 }, false);
