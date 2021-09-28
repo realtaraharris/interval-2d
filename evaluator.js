27c205b3ad6dc4f1fcaca99fdc8466fadb05e6d8
Elijah Insua <tmpvar@gmail.com>
2021-09-27 23:11:24 -0700

    write up a standalone evaluator

---
+++ evaluator.js
@@ -0,0 +1,248 @@
+var iadd = require('interval-add');
+var isub = require('interval-subtract');
+var imul = require('interval-multiply');
+var imin = require('interval-min');
+
+module.exports = evaluate
+
+var max = Math.max;
+var min = Math.min;
+function isqr(a) {
+  if (a[0]>=0.0) {
+    return [a[0] * a[0], a[1] * a[1]]
+  } else if (a[1] < 0.0) {
+    return [a[1] * a[1], a[0] * a[0]]
+  } else {
+    return [0.0, max(a[0]*a[0], a[1]*a[1])]
+  }
+}
+
+function ilensq(a, b) {
+  var pa = isqr(a)
+  var pb = isqr(b)
+
+  return [pa[0] + pb[0], pa[1] + pb[1]]
+}
+
+function isqrt(a) {
+  return [Math.sqrt(a[0]), Math.sqrt(a[1])];
+}
+
+function ilen(a, b) {
+  var pa = isqr(a);
+  var pb = isqr(b);
+  return isqrt([
+    pa[0] + pb[0],
+    pa[1] + pb[1]
+  ])
+}
+
+function iabs (i) {
+  if (i[0] >= 0.0) {
+    return i;
+  } else if (i[1] <= 0.0) {
+    return [-i[1], -i[0]];
+  } else {
+    return [0, Math.max(-i[0], i[1])];
+  }
+}
+
+function imax (a, b) {
+  if (a[1] <= b[0]) {
+    return b;
+  }
+
+  if (a[0] <= b[0]) {
+    if (b[0] <= a[1]) {
+      if (a[1] <= b[1]) {
+        return b;
+      } else {
+        return [b[0], a[1]];
+      }
+    }
+  }
+
+  if (b[1] <= a[0]) {
+    return a;
+  }
+
+  if (b[0] <= a[0]) {
+    if (a[0] <= b[1]) {
+      if (b[1] <= a[1]) {
+        return a;
+      } else {
+        return [a[0], b[1]];
+      }
+    }
+  }
+}
+
+function iset(out, l, u) {
+  out[0] = l;
+  out[1] = u;
+}
+
+function ival(v) {
+  return [v, v]
+}
+
+function ipowsmooth(a, b, k) {
+  k = k || ival(8);
+  a = ipow(a, k);
+  b = ipow(b, k);
+  return ipow(
+    idiv(imul(a, b), iadd(a, b)),
+    idiv(ival(1.0), k)
+  );
+}
+
+function idiv(a, b) {
+  var l = a[0] / b[0];
+  var u = a[1] / b[1];
+  return [min(l, u), max(l, u)]
+}
+
+function crossesZero (interval) {
+  return 0 >= interval[0] && 0 <= interval[1];
+}
+
+function middle (l, u) {
+  return (l + u) * 0.5;
+}
+
+function circle (x, y, r, translation) {
+  var lx = [x[0] - translation[0], x[1] - translation[0]];
+  var ly = [y[0] - translation[1], y[1] - translation[1]];
+
+  return isub(ilen(lx, ly), r);
+}
+
+function rect (x, y, rx, ry, p) {
+  return imax(
+    isub(iabs(isub(x, ival(p[0]))), rx),
+    isub(iabs(isub(y, ival(p[1]))), ry)
+  );
+}
+
+function triangle (x, y, w, h, translation) {
+  return imax(
+    isub(
+      imul(isub(x, h), isub(h, h)),
+      imul(isub(y, h), isub(w, h))
+    ),
+    imax(
+      isub(
+        imul(isub(x, w), isub(w, h)),
+        imul(isub(y, h), isub(h, w))
+      ),
+      isub(
+        imul(isub(x, h), isub(h, w)),
+        imul(isub(y, w), isub(h, h))
+      )
+    )
+  );
+}
+
+function opicut(a, b) {
+  var la = -a[0];
+  var ua = -a[1];
+  return imax(
+    [min(la, ua), max(la, ua)],
+    b
+  )
+}
+
+
+var inout_s = [0, 0];
+function inout(addQuad, r, ix, iy, shapes, scale) {
+  var size = Math.max(ix[1] - ix[0], iy[1] - iy[0]);
+  var leaf = r[0] <= 0 && r[1] >= 0 && size < (1 / scale)
+  addQuad(
+    ix[0],
+    iy[1],
+    ix[1] - ix[0],
+    iy[1] - iy[0],
+    shapes.length,
+    leaf
+  )
+}
+
+function evaluateScene (inputShapes, x, y, translation, outFilteredShapes) {
+  var l = inputShapes.length;
+  var r = ival(1);
+  for (var i=0; i<l; i++) {
+    var c = inputShapes[i];
+    var distanceInterval = circle(x, y, ival(c[2]), [
+      translation[0] + c[0],
+      translation[1] + c[1]
+    ]);
+
+    if (crossesZero(distanceInterval)) {
+      outFilteredShapes.push(c)
+    }
+
+    imin(distanceInterval, r, r);
+  }
+  return r;
+}
+
+var scratchx = [0, 0];
+var scratchy = [0, 0];
+function evaluate(inputShapes, translation, lx, ly, ux, uy, addQuad, scale, depth) {
+  var maxDepth = depth;
+  var size = Math.max(ux - lx, uy - ly) * scale
+
+  if (size < 1) {
+    return maxDepth;
+  }
+
+  var midx = middle(lx, ux);
+  var midy = middle(ly, uy);
+  var r;
+
+  iset(scratchx, midx, ux);
+  iset(scratchy, midy, uy);
+
+  var upperRightShapes = []
+  r = evaluateScene(inputShapes, scratchx, scratchy, translation, upperRightShapes);
+  if (crossesZero(r)) { // upper-right
+    inout(addQuad, r, scratchx, scratchy, upperRightShapes, scale);
+    maxDepth = max(maxDepth,
+      evaluate(upperRightShapes, translation, midx, midy, ux, uy, addQuad, scale, depth + 1, evaluateScene)
+    );
+  }
+
+  iset(scratchx, lx, midx);
+  iset(scratchy, midy, uy);
+  var upperLeftShapes = []
+  r = evaluateScene(inputShapes, scratchx, scratchy, translation, upperLeftShapes);
+  if (crossesZero(r)) { // upper-left
+    inout(addQuad, r, scratchx, scratchy, upperLeftShapes, scale);
+    maxDepth = max(maxDepth,
+      evaluate(upperLeftShapes, translation, lx, midy, midx, uy, addQuad, scale, depth + 1, evaluateScene)
+    );
+  }
+  iset(scratchx, lx, midx);
+  iset(scratchy, ly, midy);
+  var lowerRightShapes = [];
+  r = evaluateScene(inputShapes, scratchx, scratchy, translation, lowerRightShapes);
+  if (crossesZero(r)) { // lower-right
+    inout(addQuad, r, scratchx, scratchy, lowerRightShapes, scale);
+    maxDepth = max(maxDepth,
+      evaluate(lowerRightShapes, translation, lx, ly, midx, midy, addQuad, scale, depth + 1, evaluateScene)
+    );
+  }
+
+  iset(scratchx, midx, ux);
+  iset(scratchy, ly, midy);
+  var lowerLeftShapes = [];
+  r = evaluateScene(inputShapes, scratchx, scratchy, translation, lowerLeftShapes);
+  if (crossesZero(r)) { // lower-left
+    inout(addQuad, r, scratchx, scratchy, lowerLeftShapes, scale);
+    maxDepth = max(maxDepth,
+      evaluate(lowerLeftShapes, translation, midx, ly, ux, midy, addQuad, scale, depth + 1, evaluateScene)
+    );
+  }
+
+  return maxDepth;
+}
--- main-regl.js
+++ main-regl.js
@@ -1,15 +1,11 @@
 var fc = require('fc');
 var center = require('ctx-translate-center')
-var iadd = require('interval-add');
-var isub = require('interval-subtract');
-var imul = require('interval-multiply');
-var imin = require('interval-min');
+
 
 const regl = require('regl')()
 const mat4 = require('gl-mat4')
 
-var max = Math.max;
-var min = Math.min;
+const evaluate = require('./evaluator')
 const viewport = [0,0];
 
 const MAX_OPS = (1<<21);
@@ -28,241 +24,6 @@ const evaluatorContext = {
   addPoint(x, y) {
     this.points.push(x, y);
   }
-}
-
-
-
-function isqr(a) {
-  if (a[0]>=0.0) {
-    return [a[0] * a[0], a[1] * a[1]]
-  } else if (a[1] < 0.0) {
-    return [a[1] * a[1], a[0] * a[0]]
-  } else {
-    return [0.0, max(a[0]*a[0], a[1]*a[1])]
-  }
-}
-
-function ilensq(a, b) {
-  var pa = isqr(a)
-  var pb = isqr(b)
-
-  return [pa[0] + pb[0], pa[1] + pb[1]]
-}
-
-function isqrt(a) {
-  return [Math.sqrt(a[0]), Math.sqrt(a[1])];
-}
-
-function ilen(a, b) {
-  var pa = isqr(a);
-  var pb = isqr(b);
-  return isqrt([
-    pa[0] + pb[0],
-    pa[1] + pb[1]
-  ])
-}
-
-function iabs (i) {
-  if (i[0] >= 0.0) {
-    return i;
-  } else if (i[1] <= 0.0) {
-    return [-i[1], -i[0]];
-  } else {
-    return [0, Math.max(-i[0], i[1])];
-  }
-}
-
-function imax (a, b) {
-  if (a[1] <= b[0]) {
-    return b;
-  }
-
-  if (a[0] <= b[0]) {
-    if (b[0] <= a[1]) {
-      if (a[1] <= b[1]) {
-        return b;
-      } else {
-        return [b[0], a[1]];
-      }
-    }
-  }
-
-  if (b[1] <= a[0]) {
-    return a;
-  }
-
-  if (b[0] <= a[0]) {
-    if (a[0] <= b[1]) {
-      if (b[1] <= a[1]) {
-        return a;
-      } else {
-        return [a[0], b[1]];
-      }
-    }
-  }
-}
-
-function iset(out, l, u) {
-  out[0] = l;
-  out[1] = u;
-}
-
-function ival(v) {
-  return [v, v]
-}
-
-function ipowsmooth(a, b, k) {
-  k = k || ival(8);
-  a = ipow(a, k);
-  b = ipow(b, k);
-  return ipow(
-    idiv(imul(a, b), iadd(a, b)),
-    idiv(ival(1.0), k)
-  );
-}
-
-function idiv(a, b) {
-  var l = a[0] / b[0];
-  var u = a[1] / b[1];
-  return [min(l, u), max(l, u)]
-}
-
-function crossesZero (interval) {
-  return 0 >= interval[0] && 0 <= interval[1];
-}
-
-function middle (l, u) {
-  return (l + u) * 0.5;
-}
-
-function circle (x, y, r, translation) {
-  var lx = [x[0] - translation[0], x[1] - translation[0]];
-  var ly = [y[0] - translation[1], y[1] - translation[1]];
-
-  return isub(ilen(lx, ly), r);
-}
-
-function rect (x, y, rx, ry, p) {
-  return imax(
-    isub(iabs(isub(x, ival(p[0]))), rx),
-    isub(iabs(isub(y, ival(p[1]))), ry)
-  );
-}
-
-function triangle (x, y, w, h, translation) {
-  return imax(
-    isub(
-      imul(isub(x, h), isub(h, h)),
-      imul(isub(y, h), isub(w, h))
-    ),
-    imax(
-      isub(
-        imul(isub(x, w), isub(w, h)),
-        imul(isub(y, h), isub(h, w))
-      ),
-      isub(
-        imul(isub(x, h), isub(h, w)),
-        imul(isub(y, w), isub(h, h))
-      )
-    )
-  );
-}
-
-function opicut(a, b) {
-  var la = -a[0];
-  var ua = -a[1];
-  return imax(
-    [min(la, ua), max(la, ua)],
-    b
-  )
-}
-
-var stats = {
-  totalLeafOps: 0,
-  totalLeaves: 0,
-  opsPerLeaf: [],
-  reset() {
-    this.totalLeaves = 0;
-    this.totalLeafOps = 0;
-    this.opsPerLeaf.length = 0;
-  }
-};
-
-function inout(ctx, r, ix, iy, shapes) {
-  var size = Math.max(ix[1] - ix[0], iy[1] - iy[0]);
-  if (r[0] <= 0 && r[1] >= 0 && size < (1 / mouse.zoom)) {
-
-    stats.totalLeafOps += shapes.length
-    stats.totalLeaves++;
-    stats.opsPerLeaf.push(shapes.length);
-    // ctx.fillRect(ix[0], iy[0], (ix[1] - ix[0]), (iy[1] - iy[0]));
-    evaluatorContext.addPoint(ix[0], iy[0]);
-  } else {
-    // ctx.fillStyle = `hsla(${size}, 100%, 55%, .75)`
-  }
-  return r;
-}
-
-var scratchx = [0, 0];
-var scratchy = [0, 0];
-function box (inputShapes, translation, lx, ly, ux, uy, ctx, scale, depth, fn) {
-  var maxDepth = depth;
-  var size = Math.max(ux - lx, uy - ly) * scale
-
-  if (size < 1) {
-    return maxDepth;
-  }
-
-  var midx = middle(lx, ux);
-  var midy = middle(ly, uy);
-  var r;
-
-  iset(scratchx, midx, ux);
-  iset(scratchy, midy, uy);
-
-  var upperRightShapes = []
-  r = fn(inputShapes, scratchx, scratchy, translation, upperRightShapes);
-  if (crossesZero(r)) { // upper-right
-    inout(ctx, r, scratchx, scratchy, upperRightShapes);
-    maxDepth = max(maxDepth,
-      box(upperRightShapes, translation, midx, midy, ux, uy, ctx, scale, depth + 1, fn)
-    );
-  }
-
-  iset(scratchx, lx, midx);
-  iset(scratchy, midy, uy);
-  var upperLeftShapes = []
-  r = fn(inputShapes, scratchx, scratchy, translation, upperLeftShapes);
-  if (crossesZero(r)) { // upper-left
-    inout(ctx, r, scratchx, scratchy, upperLeftShapes);
-    maxDepth = max(maxDepth,
-      box(upperLeftShapes, translation, lx, midy, midx, uy, ctx, scale, depth + 1, fn)
-    );
-  }
-
-  iset(scratchx, lx, midx);
-  iset(scratchy, ly, midy);
-  var lowerRightShapes = [];
-  r = fn(inputShapes, scratchx, scratchy, translation, lowerRightShapes);
-  if (crossesZero(r)) { // lower-right
-    inout(ctx, r, scratchx, scratchy, lowerRightShapes);
-    maxDepth = max(maxDepth,
-      box(lowerRightShapes, translation, lx, ly, midx, midy, ctx, scale, depth + 1, fn)
-    );
-  }
-
-  iset(scratchx, midx, ux);
-  iset(scratchy, ly, midy);
-  var lowerLeftShapes = [];
-  r = fn(inputShapes, scratchx, scratchy, translation, lowerLeftShapes);
-  if (crossesZero(r)) { // lower-left
-    inout(ctx, r, scratchx, scratchy, lowerLeftShapes);
-    maxDepth = max(maxDepth,
-      box(lowerLeftShapes, translation, midx, ly, ux, midy, ctx, scale, depth + 1, fn)
-    );
-  }
-
-  return maxDepth;
 }
 
 var mouse = { zoom: 1, down: false, translate: [0, 0] }
@@ -336,6 +97,18 @@ const drawPoints = regl({
   primitive: 'points'
 })
 
+
+var stats = {
+  totalLeafOps: 0,
+  totalLeaves: 0,
+  opsPerLeaf: [],
+  reset() {
+    this.totalLeaves = 0;
+    this.totalLeafOps = 0;
+    this.opsPerLeaf.length = 0;
+  }
+};
+
 regl.frame((ctx) => {
   var w = 0.5 * ctx.viewportWidth / ctx.pixelRatio / mouse.zoom;
   var h = 0.5 * ctx.viewportHeight / ctx.pixelRatio / mouse.zoom;
@@ -355,26 +128,7 @@ regl.frame((ctx) => {
       shape[2]
     ])
 
-    function evaluateScene (inputShapes, x, y, translation, outFilteredShapes) {
-      var l = inputShapes.length;
-      var r = ival(1);
-      for (var i=0; i<l; i++) {
-        var c = inputShapes[i];
-        var distanceInterval = circle(x, y, ival(c[2]), [
-          translation[0] + c[0],
-          translation[1] + c[1]
-        ]);
-
-        if (crossesZero(distanceInterval)) {
-          outFilteredShapes.push(c)
-        }
-
-        imin(distanceInterval, r, r);
-      }
-      return r;
-    }
-
-    var maspect = max(ctx.viewportHeight, ctx.viewportWidth);
+    var maspect = Math.max(ctx.viewportHeight, ctx.viewportWidth);
     var hw = (maspect / 2) / mouse.zoom;
     var hh = (maspect / 2) / mouse.zoom;
 
@@ -386,8 +140,27 @@ regl.frame((ctx) => {
     var ux =  hw;
     var uy =  hh;
 
-
-    box(jitteredShapes, translation, lx, ly, ux, uy, ctx, mouse.zoom, 0, evaluateScene);
+    evaluate(
+      jitteredShapes,
+      translation,
+      lx,
+      ly,
+      ux,
+      uy,
+      (x, y, size, opCount, leaf) => {
+        if (leaf) {
+          stats.totalLeafOps += opCount
+          stats.totalLeaves++;
+          stats.opsPerLeaf.push(opCount);
+          evaluatorContext.addPoint(x, y);
+        } else {
+          //  TODO: add a colored quad
+          // addQuad.fillStyle = `hsla(${size}, 100%, 55%, .75)`
+        }
+      },
+      mouse.zoom,
+      0
+    );
   }
   const end = performance.now();
 
